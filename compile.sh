4dc43bf59f58e5dd203a3ceba2936e61b2804c0b
David Halls <dahalls@gmail.com>
2012-10-18 08:03:11 +0100

    Tidy up diff against hulihan script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 ruhoh compile
+rm -f compiled/assets/my_hooligan/*.sh
--- themes/my_hooligan/diff_against_dhulihan.sh
+++ themes/my_hooligan/diff_against_dhulihan.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # assumes dhulihan's Jekyll-Bootstrap theme is in ~/Downloads/hooligan
-diff -rub /home/david/Downloads/hooligan/assets/themes/hooligan/_sass stylesheets/_sass/
+# clone it from here: https://github.com/dhulihan/hooligan.git
+diff -rub "$HOME/Downloads/hooligan/assets/themes/hooligan/_sass" stylesheets/_sass/
