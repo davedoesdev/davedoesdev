f164ba91527a1bec20496c6b77e7407dbedd25ad
David Halls <dahalls@gmail.com>
2013-07-08 23:38:05 +0100

    Make hooligan theme a submodule

--- themes/my_hooligan/diff_against_dhulihan.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# assumes dhulihan's Jekyll-Bootstrap theme is in ~/Downloads/hooligan
-# clone it from here: https://github.com/dhulihan/hooligan.git
-diff -rub "$HOME/Downloads/hooligan/assets/themes/hooligan/_sass" stylesheets/_sass/
