9008b216360936207d3dab21a01824eb60088dd0
David Halls <dahalls@gmail.com>
2013-07-09 14:28:23 +0100

    hooligan updates and renamed repos

--- deploy_to_github_pages.sh
+++ deploy_to_github_pages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-./compile.sh && rm -rf ../davedoesdev.github.com/* && cp -r compiled/* ../davedoesdev.github.com
+./compile.sh && rm -rf ../davedoesdev.github.io/* && cp -r compiled/* ../davedoesdev.github.io && echo www.davedoesdev.com > ../davedoesdev.github.io/CNAME
